c7bef3ee5fc1b6825c3faf1263ab7e61b9134e77
Emiliano Cocom <joseemilianococom@gmail.com>
2021-12-11 15:37:24 -0600

    Entry components

--- src/modules/daybook/components/EntryList.vue
+++ src/modules/daybook/components/EntryList.vue
@@ -10,7 +10,7 @@
     
 
         <div class="entry-scrollarea">
-            <Entry v-for="item in entriesByTerm" :key="item" />
+            <Entry v-for="entry in entriesByTerm" :key="entry.id" :entry="entry" />
         </div>
     </div>
 </template>
