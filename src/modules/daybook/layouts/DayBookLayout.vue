6945965cbd00c2dc9353b87a46ff352bf9d48977
Emiliano Cocom <joseemilianococom@gmail.com>
2021-12-11 19:56:31 -0600

    load entries from backend

--- src/modules/daybook/layouts/DayBookLayout.vue
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -12,12 +12,19 @@
 </template>
 
 <script>
-import {defineAsyncComponent} from 'vue'
+import { defineAsyncComponent } from 'vue'
+import { mapActions } from 'vuex'
 
 export default {
     components: {
         Navbar: defineAsyncComponent( () => import(/* webpackChunkName: "Navbar" */ '@/modules/daybook/components/Navbar')),
         EntryList: defineAsyncComponent( () => import(/* webpackChunkName: "EntryList" */ '@/modules/daybook/components/EntryList')),
+    },
+    methods: {
+        ...mapActions('journal', ['loadEntries'])
+    },
+    created() {
+        this.loadEntries()
     }
 }
 </script>
